b43165f4ad9397ff15113f40d8d40b78f86fef30
KamilKhametov <khametovkamil93@gmail.com>
2020-07-04 14:57:01 +0300

    Java Commit & Youtube Video

---
+++ src/com/company/Main.java
@@ -0,0 +1,23 @@
+package com.company;
+
+import java.awt.*;
+import java.io.IOException;
+import java.net.URI;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+	Scanner sc = new Scanner(System.in);
+        System.out.println("Введите URL-адрес");
+        StringBuffer url = new StringBuffer(sc.next());
+        url.insert(12, "ss");
+
+        Desktop desktop = Desktop.getDesktop();
+        try{
+            desktop.browse(URI.create(url.toString()));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
